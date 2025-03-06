7c2ddb664f7491cb034929d8224f3f86fd5c3de3
Botta Michele <72565868+bottamichele@users.noreply.github.com>
2025-03-06 18:14:46 +0100

    Update nn_discrete_action.py

--- test/nn_discrete_action.py
+++ test/nn_discrete_action.py
@@ -4,7 +4,7 @@ import torch as tc
 
 from torch.optim import Adam
 
-from gymnasium.vector import SyncVectorEnv
+from gymnasium.wrappers.vector import RecordEpisodeStatistics
 
 from ppo_algorithm import ActorCritic, Buffer
 from ppo_algorithm.train import train_policy_da, ppo_train_step
@@ -13,12 +13,12 @@ from ppo_algorithm.train import train_policy_da, ppo_train_step
 # ============ HYPERPARAMETERS ===========
 # ========================================
 
-TARGET_TOTAL_STEPS = 250000
+TARGET_TOTAL_STEPS = 50000
 N_ACTORS = 1
-STEPS = 500
+N_STEPS = 250
 GAMMA = 0.99
 GAE_COEFFICIENT = 0.95
-NORMALIZE_ADVANTAGE = True
+NORMALIZE_ADVANTAGE = False
 BATCH_SIZE = 64
 MAX_GRADIENT_NORM = 0.5
 LEARNING_RATE = 10**-3
@@ -35,50 +35,59 @@ DEVICE = tc.device("cpu")
 
 if __name__ == "__main__":
     #Create enviroment.
-    envs = gym.make_vec("LunarLander", num_envs=N_ACTORS, vectorization_mode="sync")
+    envs = gym.make_vec("CartPole-v1", num_envs=N_ACTORS, vectorization_mode="sync")
+    envs = RecordEpisodeStatistics(envs, 1)
     OBSERVATION_SIZE = envs.single_observation_space.shape[0]
     ACTION_SIZE = envs.single_action_space.n
 
     #Create ActorCritic net.
-    model = ActorCritic(OBSERVATION_SIZE, ACTION_SIZE, 256, 1, 1, 1)
+    model = ActorCritic(OBSERVATION_SIZE, ACTION_SIZE, 256, 1, 1, 1).to(device=DEVICE)
 
     #Create optimizer
     optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
 
     #Create buffer.
-    buffer = Buffer(STEPS, N_ACTORS, OBSERVATION_SIZE, 1, act_dtype=tc.int32, device=DEVICE)
+    buffer = Buffer(N_STEPS, N_ACTORS, OBSERVATION_SIZE, 1, act_dtype=tc.int32, device=DEVICE)
 
     #Training phase.
     scores = []
     total_states = 0
-    obs, _ = envs.reset()
+    obs, infos = envs.reset()
+    done = np.zeros(N_ACTORS, dtype=np.int32)
+
+    print("Train Phase")
 
     while total_states <= TARGET_TOTAL_STEPS:
-        for _ in range(STEPS):
+        for _ in range(N_STEPS):
             obs = tc.from_numpy(obs).to(device=DEVICE)
+            done = tc.from_numpy(done).to(dtype=tc.int32, device=DEVICE)
 
             #Choose action and compute log probability
             with tc.no_grad():
                 action, value, action_dist = train_policy_da(model, obs)
                 log_prob = action_dist.log_prob(action)
 
-                #Reshape dimensions
-                action = action.reshape(-1)
-                value = value.reshape(-1)
-
             #Perform action chosen.
-            next_obs, r, terminated, truncation, _ = envs.step(action.cpu().to(dtype=tc.int32).numpy())
-
-            done = tc.from_numpy(np.logical_or(terminated, truncation)).to(dtype=np.int32, device=DEVICE)
+            next_obs, r, terminated, truncation, infos = envs.step(action.reshape(-1).cpu().to(dtype=tc.int32).numpy())
             reward = tc.from_numpy(r).to(device=DEVICE)
 
             #Store one step infos into buffer.
-            buffer.store(obs, action, log_prob, reward, done, value)
+            buffer.store(obs, action, log_prob, reward, done, value.reshape(-1))
 
             #Next observation.
             obs = next_obs
+            done = np.logical_or(terminated, truncation)
             total_states += 1
 
+            if "episode" in infos:
+                print("- state = {:>6d}; cum. reward = {}".format(total_states, infos["episode"]["r"]))
+
+        #Compute advantages and returns.
+        with tc.no_grad():
+            _, value = model(tc.from_numpy(obs).to(device=DEVICE))
+        buffer.compute_advantage_and_return(value.reshape(-1), tc.from_numpy(done).to(dtype=tc.int32, device=DEVICE))
+
+        #Train step.
         ppo_train_step(model, 
                        train_policy_da, 
                        buffer, 
